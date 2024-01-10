66223c1c448329294c994ad07c7cc5ff40651b1f
Karthikkv1 <karthik.k.v.036@gmail.com>
2024-01-10 10:46:06 +0530

    product section created

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- Bootstrap CSS -->
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<!-- Font awesome link -->
+    <!-- Font awesome link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-<!-- External css -->
-    <link rel="stylesheet" href="./style.css">    
+    <!-- External css -->
+    <link rel="stylesheet" href="./style.css">
 
     <title>My store</title>
 </head>
@@ -21,7 +21,7 @@
 
     <div class="container-fluid p-0">
 
-        <nav class="navbar navbar-expand-lg  bg-info">
+        <nav class="navbar navbar-expand-lg  bg-info navbar-light">
             <div class="container-fluid">
                 <img src="./Images/logo.png" alt="" class="logo">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -59,6 +59,39 @@
             </div>
         </nav>
 
+        <!-- Second child --> <!-- 10-01-2024 -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+
+            <ul class="navbar-nav me-auto">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Welcome Guest</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+
+            </ul>
+        </nav>
+
+        <!-- Third child --> <!-- 10-01-2024 -->
+        <div class="bg-light">
+            <h3 class="text-center">Hidden Store</h3>
+            <p class="text-center">Communication is at the heart of e-commerce and community</p> 
+        </div>
+
+        <!-- Fourth Child -->
+        <div class="row">
+            <div class="col-md-10">
+                products
+            </div>
+
+            <div class="col-md-2">
+                sidenav
+            </div>
+        </div>
+
         <!-- <div class="bg-info p-3 text-center">
             <p>All rights reserved Designed by Karthik 2024</P>
        </div> -->
@@ -68,7 +101,7 @@
 
 
 
-<!-- Bootstrap JS -->
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
@@ -76,5 +109,3 @@
 </body>
 
 </html>
-
-
