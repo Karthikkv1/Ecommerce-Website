6f8612de05b8d6f40b3a19f141d93b7050d2cc90
Karthikkv1 <karthik.k.v.036@gmail.com>
2024-01-18 22:37:38 +0530

    database created and connected succesfully

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -1,3 +1,22 @@
+
+<?php
+
+   include('../includes/connect.php');       //18-1-2024 10:05 PM
+   if(isset($_POST['insert_cat'])){
+       $category_title = $_POST['cat_title'];  //To access value
+       $insert_query = "insert into `categories` (category_title) values ('$category_title') ";  // Insert the data into the database. database ,column name
+       $result=mysqli_query($con,$insert_query);  //To execute query
+       if($result){
+        echo "<script>alert('category has been inserted successfully')</script>";
+       }
+    }
+
+
+?>
+
+
+
+
 <form action="" method="post" class="mb-2">
     <div class="input-group w-90 mb-2">
         <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
@@ -7,7 +26,7 @@
 
     <div class="input-group w-10 mb-2 m-auto">
        
-        <!-- <input type="submit" class="form-control bg-info" name="insert_cat" value="Insert Categories"  > -->
-        <button class="bg-info p-2 my-3 border-0">Insert Categories</button>
+        <input type="submit" class="bg-info p-2 my-3 border-0" name="insert_cat" value="Insert Categories"  >
+        <!-- <button class="bg-info p-2 my-3 border-0">Insert Categories</button> -->
     </div>
 </form>
