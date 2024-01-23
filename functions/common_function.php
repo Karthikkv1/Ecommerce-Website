3e517896d5e4288bbc3709b46668aa4666a57699
Karthikkv1 <karthik.k.v.036@gmail.com>
2024-01-23 14:49:43 +0530

    set

--- functions/common_function.php
+++ functions/common_function.php
@@ -58,6 +58,60 @@ function getproducts(){
 }
 }
 
+//getting unique categories
+function get_unique_categories(){
+
+    global $con; //23-01-2024
+
+    //condition to check isset or not
+    if(isset($_GET['category'])){
+        $category_id=$_GET['category']; //23-01-2024
+        
+            
+       
+
+    
+
+
+
+    $select_query="Select * from `products` order by rand() limit 0,9";
+    $result_query=mysqli_query($con,$select_query);
+    // $row=mysqli_fetch_assoc($result_query);
+    // echo $row['product_title'];
+    while($row=mysqli_fetch_assoc($result_query))
+    {
+        $product_id = $row['product_id']; //product_id as mentioned in database
+        $product_title = $row['product_title'];
+        $product_description = $row['product_description'];
+        // $product_keywords = $row['product_keywords'];
+        $product_image1 = $row['product_image1'];
+        $product_price = $row['product_price'];
+        $category_id = $row['category_id'];
+        $brand_id = $row['brand_id'];
+
+        echo "  <div class='col-md-4 mb-2'>
+        <div class='card'>
+            <img src='./admin_area/product_images/ $product_image1' class='card-img-top' alt='$product_title'> 
+            <div class='card-body'>
+                <h5 class='card-title'>$product_title</h5>
+                <p class='card-text'>$product_description
+           </p>
+                <a href='#' class='btn btn-info'>Add to cart</a>
+                <a href='#' class='btn btn-secondary'>View More</a>
+            </div>
+        </div>
+    </div>";
+        // echo "<br>";
+        
+        // $product_image2 = $row['product_image2'];
+        // $product_image3 = $row['product_image3'];
+    }
+
+}
+}
+
+
+
 //displaying brands in side nav
 
 function getbrands(){
