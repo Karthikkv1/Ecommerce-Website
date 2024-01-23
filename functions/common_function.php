08fc73bbb1383dec968e45ee2f81c651e1c185c6
Karthikkv1 <karthik.k.v.036@gmail.com>
2024-01-23 15:09:56 +0530

    no brand available

--- functions/common_function.php
+++ functions/common_function.php
@@ -119,6 +119,67 @@ function get_unique_categories(){
 }
 
 
+//getting unique brands
+function get_unique_brands(){
+
+    global $con; //23-01-2024
+
+    //condition to check isset or not
+    if(isset($_GET['brand'])){ //'brand' as mentioned in url when clicking on particular brand for example brand=1
+        $brand_id=$_GET['brand_id']; //23-01-2024
+        
+            
+       
+
+    
+
+
+
+    $select_query="Select * from `products` where category_id=$brand_id";
+    $result_query=mysqli_query($con,$select_query);
+    $num_of_rows=mysqli_num_rows( $result_query);
+
+    //displaying error if searched product is not there //23-01-2024
+    if($num_of_rows==0){
+        echo "<h2 class='text-center text-danger' >This brand is not available for service</h2>";
+    }
+
+
+    // $row=mysqli_fetch_assoc($result_query);
+    // echo $row['product_title'];
+    while($row=mysqli_fetch_assoc($result_query))
+    {
+        $product_id = $row['product_id']; //product_id as mentioned in database
+        $product_title = $row['product_title'];
+        $product_description = $row['product_description'];
+        // $product_keywords = $row['product_keywords'];
+        $product_image1 = $row['product_image1'];
+        $product_price = $row['product_price'];
+        $category_id = $row['category_id'];
+        $brand_id = $row['brand_id'];
+
+        echo "  <div class='col-md-4 mb-2'>
+        <div class='card'>
+            <img src='./admin_area/product_images/ $product_image1' class='card-img-top' alt='$product_title'> 
+            <div class='card-body'>
+                <h5 class='card-title'>$product_title</h5>
+                <p class='card-text'>$product_description
+           </p>
+                <a href='#' class='btn btn-info'>Add to cart</a>
+                <a href='#' class='btn btn-secondary'>View More</a>
+            </div>
+        </div>
+    </div>";
+        // echo "<br>";
+        
+        // $product_image2 = $row['product_image2'];
+        // $product_image3 = $row['product_image3'];
+    }
+
+}
+}
+
+
 
 //displaying brands in side nav
 
