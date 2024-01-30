7465cc2d0d84aaa9af6dba700e5d004d63930b0a
Karthikkv1 <karthik.k.v.036@gmail.com>
2024-01-30 21:53:09 +0530

    User Login form modified1

--- users_area/user_login.php
+++ users_area/user_login.php
@@ -17,7 +17,7 @@
 <body>
     <div class="container-fluid my-3">
         <h2 class="text-center">New User Registration</h2>
-        <div class="row d-flex align-items-center justify-content-center"> <!--30-01-2024-->
+        <div class="row d-flex align-items-center justify-content-center mt-5"> <!--30-01-2024-->
             <div class="col-lg-12 col-xl-6">
                 <form action="" method="post" enctype="multipart/form-data">
                     <!-- User Name Field -->
@@ -40,7 +40,7 @@
 
 
                     <div class="mt-4 pt-2">
-                        <input type="submit" value="Register" class="bg-info py-2 px-3 border-0" name="user_register">
+                        <input type="submit" value="Login" class="bg-info py-2 px-3 border-0" name="user_login">
                         <p class="small fw-bold mt-2 pt-1 mb-0">Already have an account ?<a href="user_login.php"
                                 class="text-danger"> Login </a></p>
                     </div>
