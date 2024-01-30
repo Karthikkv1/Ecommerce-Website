c19951fc11c47e679dd74f903982f6a8e68e7af5
Karthikkv1 <karthik.k.v.036@gmail.com>
2024-01-30 21:27:50 +0530

    User Login Form

---
+++ users_area/user_registration.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Registration</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+
+
+</head>
+
+<body>
+    <div class="container-fluid my-3">
+        <h2 class="text-center">New User Registration</h2>
+        <div class="row d-flex align-items-center justify-content-center"> <!--30-01-2024-->
+            <div class="col-lg-12 col-xl-6">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <!-- User Name Field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_username" class="form-label">Username</label>
+                        <input type="text" id="user_username" class="form-control" placeholder="Enter Your UserName"
+                            autocomplete="off" required="required" name="user_username">
+                    </div>
+
+                    <!-- Email  Field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_email" class="form-label">Email</label>
+                        <input type="email" id="user_email" class="form-control" placeholder="Enter Your Email"
+                            autocomplete="off" required="required" name="user_email">
+                    </div>
+
+                    <!-- Image  Field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_image" class="form-label">User Image</label>
+                        <input type="file" id="user_image" class="form-control" required="required" name="user_image">
+                    </div>
+
+                    <!-- Password Field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_password" class="form-label">Password</label>
+                        <input type="password" id="user_password" class="form-control" placeholder="Enter Your Password"
+                            autocomplete="off" required="required" name="user_password">
+                    </div>
+
+                    <!--Confirm Password Field -->
+                    <div class="form-outline mb-4">
+                        <label for="conf_user_password" class="form-label">Confirm Password</label>
+                        <input type="password" id="conf_user_password" class="form-control"
+                            placeholder="Confirm Your Password" autocomplete="off" required="required"
+                            name="conf_user_password">
+                    </div>
+
+                    <!-- Address Field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_address" class="form-label">Address</label>
+                        <input type="text" id="user_address" class="form-control" placeholder="Enter Your Address"
+                            autocomplete="off" required="required" name="user_address">
+                    </div>
+
+                    <!-- Contact Field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_contact" class="form-label">Contact</label>
+                        <input type="text" id="user_contact" class="form-control" placeholder="Enter Your Mobile Number"
+                            autocomplete="off" required="required" name="user_contact">
+                    </div>
+
+                    <div class="mt-4 pt-2">
+                        <input type="submit" value="Register" class="bg-info py-2 px-3 border-0" name="user_register">
+                        <p class="small fw-bold mt-2 pt-1 mb-0">Already have an account ?<a href="user_login.php" class="text-danger"> Login </a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
