c353da1d25a99999c882a70ae56f24164eac4fa3
Karthikkv1 <karthik.k.v.036@gmail.com>
2024-01-30 21:50:44 +0530

    User Login form modified

--- users_area/user_login.php
+++ users_area/user_login.php
@@ -1,11 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>User Registration</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+
+
 </head>
+
 <body>
-    <h1>User Login</h1>
+    <div class="container-fluid my-3">
+        <h2 class="text-center">New User Registration</h2>
+        <div class="row d-flex align-items-center justify-content-center"> <!--30-01-2024-->
+            <div class="col-lg-12 col-xl-6">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <!-- User Name Field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_username" class="form-label">Username</label>
+                        <input type="text" id="user_username" class="form-control" placeholder="Enter Your UserName"
+                            autocomplete="off" required="required" name="user_username">
+                    </div>
+
+
+
+
+                    <!-- Password Field -->
+                    <div class="form-outline mb-4">
+                        <label for="user_password" class="form-label">Password</label>
+                        <input type="password" id="user_password" class="form-control" placeholder="Enter Your Password"
+                            autocomplete="off" required="required" name="user_password">
+                    </div>
+
+
+
+                    <div class="mt-4 pt-2">
+                        <input type="submit" value="Register" class="bg-info py-2 px-3 border-0" name="user_register">
+                        <p class="small fw-bold mt-2 pt-1 mb-0">Already have an account ?<a href="user_login.php"
+                                class="text-danger"> Login </a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
