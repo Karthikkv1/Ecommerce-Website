f4526c11e338cd7e4c51310113830248dba9a029
Karthikkv1 <karthik.k.v.036@gmail.com>
2024-02-14 21:52:36 +0530

    order edit 11

--- users_area/order.php
+++ users_area/order.php
@@ -14,8 +14,10 @@ $total_price=0;
 $cart_query_price="Select * from `cart_details` where ip_address='$get_ip_address'";
 
 $result_cart_price=mysqli_query($con,$cart_query_price);
+
 $invoice_number=mt_rand(); //14-02-2024 //9:49PM
 echo $invoice_number; //14-02-2024 //9:49PM
+
 $count_products=mysqli_num_rows($result_cart_price);
 
 while($row_price=mysqli_fetch_array($result_cart_price)){
