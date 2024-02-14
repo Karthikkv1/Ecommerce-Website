6b71d3fcb7a6d2eb4869002761f86ce0c634dec8
Karthikkv1 <karthik.k.v.036@gmail.com>
2024-02-14 22:32:08 +0530

    order edit 15

--- users_area/order.php
+++ users_area/order.php
@@ -32,10 +32,34 @@ while($row_price=mysqli_fetch_array($result_cart_price)){
     while($row_product_price=mysqli_fetch_array($run_price))
     {
         $product_price=array($row_product_price['product_price']);
-        $product_value=array_sum($product_price);
-        $total_price+= $product_value;
+        $product_values=array_sum($product_price);
+        $total_price+= $product_values;
         
     }
 
 }
+
+//getting quantity from cart //14-02-2024 //10:02PM
+$get_cart="select * from `cart_details`";
+$run_cart=mysqli_query($con,$get_cart);
+$get_item_quantity=mysqli_fetch_array($run_cart);
+$quantity=$get_item_quantity['quantity'];  //To get quantity of product
+
+if($quantity==0){
+    $quantity=1;
+    $subtotal=$total_price;
+}
+else{
+    $quantity=$quantity;
+    $subtotal=$total_price*$quantity;
+}
+
+$insert_orders="Insert into `user_orders` (user_id,amount_due,invoice_number,total_products,order_date,order_status) values($user_id,$subtotal,$invoice_number,$count_products,NOW(),'$status')";
+$result_query=mysqli_query($con,$insert_orders);
+
+if($result_query){
+    echo "<script>alert('Orders are submitted successfully')</script>";
+    echo "<script>window.open('profile.php','_self')</script>";
+}
+
 ?>
